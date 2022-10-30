6f26dff0bb005a15ffa590b5062ac14a9520b7e2
MuhammadYousufHere <khanyousufmy80@gmail.com>
2022-10-30 09:01:46 -0700

    button disable test

--- src/components/Button/__test__/Button.spec.js
+++ src/components/Button/__test__/Button.spec.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import Button from '../Button';
 
 test('Label of button', function () {
-  const {} = render(<Button title='Click me' />);
+  const view = render(
+    <Button
+      title='Click me'
+      onClick={() => console.log('Clicked')}
+    />
+  );
   const title = screen.getByTitle(/Click me/i);
   expect(title).toBeTruthy();
+
+  // The button should be clicked
+  const btn = screen.getByRole('button');
+
+  fireEvent.click(btn);
 });
 
 // Slelect element from component
@@ -14,3 +24,19 @@ test('Label of button', function () {
 // ---> getBy
 // ---> queryBy
 // ---> findBy
+
+// DOM
+// ---> getByRole
+
+// ---> queryByRole
+// ---> findByRole
+// ---> queryByRole('button')
+// ---> findByRole('button')
+// ---> queryByRole('button', { exact: false })
+
+test('Check for disabled button', function () {
+  const button = screen.getByRole('button', { name: 'Disabled' });
+  expect(button).toBeDisabled();
+  expect(button).toHaveAttribute('disabled');
+  expect(button).toHaveAttribute('aria-disabled');
+});
