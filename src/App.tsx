d88d04445271680c5bd77c8183b9e9751b9f9189
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-10-08 21:48:41 +0500

    test App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ function App() {
   return (
     <div>
       <h1>Hello world! I am using React</h1>
+      <div className='input-group'>
+        <label htmlFor='love'>
+          Do You Love Testing?
+          <input type='checkbox' name='love' id='love' />
+        </label>
+      </div>
     </div>
   )
 }
