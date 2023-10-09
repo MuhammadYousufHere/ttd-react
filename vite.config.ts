af79fe5d8e8bddf5cf9dc0e426ae26f608b29299
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-10-09 19:08:40 +0500

    feat: fix import.meta issue with jest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 3000 },
+export default defineConfig(props => {
+  const env = loadEnv(props.mode, process.cwd(), 'VITE_APP')
+  const envWithProcessPrefix = {
+    'process.env': `${JSON.stringify(env)}`
+  }
+  return {
+    plugins: [react()],
+    define: envWithProcessPrefix,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src/'),
+        pages: path.resolve(__dirname, './src/pages/'),
+        components: path.resolve(__dirname, './src/components/'),
+        assets: path.resolve(__dirname, './src/assets/'),
+        '@types': path.resolve(__dirname, './src/@types/')
+      }
+    },
+    server: { port: 3000 },
+    build: {
+      outDir: 'build',
+      minify: false,
+      sourcemap: false
+    }
+  }
 })
