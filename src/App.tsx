5299f24769a2ee23bad8cd20e567f01250958116
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-10-09 16:45:21 +0500

    chores: add redux packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import BrowserRouter from './common/BrowserRouter'
 import { history } from './common/history'
+import AppRoutes from './routes/AppRoutes'
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter history={history}>
-      <h1>Hello world! I am using React</h1>
-      <div className='input-group'>
-        <label htmlFor='love'>
-          Do You Love Testing?
-          <input type='checkbox' name='love' id='love' />
-        </label>
-      </div>
+      <AppRoutes />
     </BrowserRouter>
   )
 }
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,8 @@
+import { Routes } from 'react-router-dom'
+import BrowserRouter from '../common/BrowserRouter'
+import { history } from '../common/history'
+import App from '../App'
+
+export default function AppRoutes() {
+  return <Routes></Routes>
+}
