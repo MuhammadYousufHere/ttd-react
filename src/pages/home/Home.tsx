f098332848ce4cf2b9868d1957bb201ee1968285
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-10-21 17:08:15 +0500

    render task1

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,10 @@
+import TaskPutItems from '../task1'
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello world! I am using React</h1>
+      {/* <h1>Hello world! I am using React</h1> */}
+      <TaskPutItems />
     </div>
   )
 }
